84a5db787791360eb5553aa40a29403ac23517c0
Olesya <100337737+olesyaay@users.noreply.github.com>
2023-05-24 15:01:27 +0300

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,8 @@ function Footer() {
             <div className="footer-copyright">
                 <div className="container">
                     © {new Date().getFullYear()} | prod by Bagaeva Olesya for RTU MIREA
-                    <a className="grey-text text-lighten-4 right" href="#!">Repository</a>
+                    <a className="grey-text text-lighten-4 right" href="https://github.com/olesyaay/SPA-react-project
+" target="_blank">Repository</a>
                 </div>
             </div>
         </div>
